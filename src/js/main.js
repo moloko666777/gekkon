70d741cc8c604759f562d9e071b2f7090ccf7ecc
moloko666777 <moloko666777@gmail.com>
2022-02-07 18:06:10 +0200

    style for main page

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,59 @@ var mySwiper = new Swiper ('.swiper-container', {
     },
 })
 
+document.addEventListener('click', function (event) {
+
+    if (!event.target.classList.contains('accordion-toggle')) return;
+
+    var content = document.querySelector(event.target.hash);
+    if (!content) return;
+
+    event.preventDefault();
+
+    let toggleIcon
+    if (content) toggleIcon = event.target.querySelector(".toggle-icon");
+
+    if (toggleIcon.classList.contains('active')) {
+        toggleIcon.classList.remove('active')
+    } else {
+        console.log(document.querySelector('.toggle-icon.active'))
+        let activeIcon = document.querySelector('.toggle-icon.active')
+
+        activeIcon ? activeIcon.classList.remove('active') : false
+
+        toggleIcon.classList.add('active')
+    }
+
+    if (content.classList.contains('active')) {
+        content.classList.remove('active');
+        return;
+    }
+
+    var accordions = document.querySelectorAll('.accordion-content.active');
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].classList.remove('active');
+    }
+
+    content.classList.toggle('active');
+
+    // for (var i = 0; i < toggleIcon.length; i++) {
+    //     toggleIcon[i].classList.remove('active');
+    // }
+    // toggleIcon.classList.toggle("active");
+
+})
+
+// document.addEventListener('click', function (event){
+//
+//     event.preventDefault();
+//
+//
+//     let toggleIcon = document.querySelector(".toggle-icon");
+//
+//     for (var i = 0; i < toggleIcon.length; i++) {
+//         toggleIcon[i].classList.remove('active');
+//     }
+//     toggleIcon.classList.toggle("active");
+// })
+
 document.getElementById('year').innerHTML = new Date().getFullYear();
